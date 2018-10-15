577172b9e14c407015caeb90bf6fae4c48efac15
Nadtochii Taras <taras.nadtochii@gmail.com>
2018-10-15 17:40:03 +0300

    Added Assert for verifying the SignIn button existing Addded steps for entering login name, password and clicking SignIn button Added Assert for verifying that Homepage is opened

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,8 @@
+import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -26,8 +30,25 @@ public class LoginTest {
         WebDriver webDriver = new FirefoxDriver();
 
         webDriver.get("https://www.linkedin.com/");
-        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/", "Homepage URL is wrong.");
-        //TODO: add asserts and steps
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/", "Login page URL is wrong.");
+
+        WebElement signInButton = webDriver.findElement(By.xpath("//input[@id='login-submit']"));
+        boolean isPresentSignInButton = webDriver.findElements(By.xpath("//input[@id='login-submit']")).size() > 0;
+        Assert.assertEquals(isPresentSignInButton, true, "There is no Sign in button on the page");
+
+
+        WebElement loginNameField = webDriver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement loginPassField = webDriver.findElement(By.xpath("//input[@id='login-password']"));
+        String loginName = "taras.nadtochii@gmail.com";
+        String loginPassword = "Taratest";
+        loginNameField.sendKeys(loginName);
+        loginPassField.sendKeys(loginPassword);
+        signInButton.click();
+
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Homepage URL is wrong");
+
+
+
 
         webDriver.quit();
 
